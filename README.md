a636d482af7f2b14a940bd50eb096e87c36c2edf
yyds-jkl <128694532+yyds-jkl@users.noreply.github.com>
2023-03-23 18:10:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# azurlane_anti_name
+
+反和谐船名:μ兵装,小XX,重樱,铁血,白鹰,META
+
+B服右侧Releases下载
+
+MUMU 下载的应该是9 游版,也在右侧Releases下载
+
+其他渠道服暂时找不到直链,无能为力.
---
+++ patch_qudao.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Download apkeep
+get_artifact_download_url () {
+    # Usage: get_download_url <repo_name> <artifact_name> <file_type>
+    local api_url="https://api.github.com/repos/$1/releases/latest"
+    local result=$(curl $api_url | jq ".assets[] | select(.name | contains(\"$2\") and contains(\"$3\") and (contains(\".sig\") | not)) | .browser_download_url")
+    echo ${result:1:-1}
+}
+
+# Artifacts associative array aka dictionary
+declare -A artifacts
+
+artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
+artifacts["apktool.jar"]="iBotPeaches/Apktool apktool .jar"
+
+# Fetch all the dependencies
+for artifact in "${!artifacts[@]}"; do
+    if [ ! -f $artifact ]; then
+        echo "Downloading $artifact"
+        curl -L -o $artifact $(get_artifact_download_url ${artifacts[$artifact]})
+    fi
+done
+
+chmod +x apkeep
+
+# Download Azur Lane
+# Download Azur Lane
+download_azurlane () {
+    if [ ! -f "AzurLane.apk" ]; then
+    # 这个链接是MUMU下载的,应该是9游,其他渠道自行修改直链
+    url="https://downali.game.uc.cn/s1/2/10/20230213150150_blhx_uc_2022_11_02_18_24_01.apk?x-oss-process=udf/uc-apk,ZBHDhDR0LVBkTsK*wpLCng==afae37c2a88fd1ca&sh=10&sf=1831727323&vh=18330f93bd450707942ce0b882a0c6b2&cc=2521889677&did=217b729f8a4841dd81901816dfba551f"
+    # 使用wget命令下载apk文件
+    curl -o blhx.apk  $url
+    fi
+}
+
+if [ ! -f "AzurLane.apk" ]; then
+    echo "Get Azur Lane apk"
+    download_azurlane
+    mv *.apk "AzurLane.apk"
+fi
+
+
+echo "Decompile Azur Lane apk"
+java -jar apktool.jar  -f d AzurLane.apk
+
+echo "Copy libs"
+cp -r libs/. AzurLane/lib/
+
+echo "Patching Azur Lane"
+oncreate=$(grep -n -m 1 'onCreate' AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
+sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+
+echo "Build Patched Azur Lane apk"
+java -jar apktool.jar  -f b AzurLane -o AzurLane.patched.apk
+
+echo "Set Github Release version"
+
+echo "PERSEUS_VERSION=$(echo Ship_Name)" >> $GITHUB_ENV
+
+mkdir -p build
+mv *.patched.apk ./build/
+find . -name "*.apk" -print
